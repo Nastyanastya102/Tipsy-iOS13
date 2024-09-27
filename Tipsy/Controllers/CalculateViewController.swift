f2a9cf0a8280bfbff780bfdafb894457aab6540e
StacyKlyashtorna <96352264+StacyKlyashtorna@users.noreply.github.com>
2024-09-27 16:34:31 -0700

    Pass props

--- Tipsy/Controllers/CalculateViewController.swift
+++ Tipsy/Controllers/CalculateViewController.swift
@@ -8,22 +8,50 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var billTextField: UITextField!
- 
+    
+    var numberOfPeople = 2
+    var tip = 0.10
+    var billTotal = 0.0
+    var finalResult = "0.0"
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
         sender.isSelected = true
+        let buttonTitle = sender.currentTitle!
+        let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        tip = buttonTitleAsANumber / 100
 
     }
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
+         let bill = billTextField.text!
+         if bill != "" {
+             billTotal = Double(bill)!
+             let result = billTotal * (1 + tip) / Double(numberOfPeople)
+             finalResult = String(format: "%.2f", result)
+         }
        performSegue(withIdentifier: "goToResult", sender: self)
     }
+    
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        if (sender.value < 12) {
-            splitNumberLabel.text = String(format: "%.0f", sender.value)
-        }
+       splitNumberLabel.text = String(format: "%.0f", sender.value)
+       numberOfPeople = Int(sender.value)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           
+           if segue.identifier == "goToResult" {
+               let destinationVC = segue.destination as! ResultsViewController
+               destinationVC.result = finalResult
+               destinationVC.tip = Int(tip * 100)
+               destinationVC.split = numberOfPeople
+           }
+       }
 }
 
 
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(result)
+        print(split)
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+
     }
 }
